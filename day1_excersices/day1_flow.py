c19debf773ec5fa04189c6fc6017d0a0659e119a
shankar1371 <shankar.uma1371@gmail.com>
2025-09-07 10:32:26 -0500

    Day1: python refresher setup

---
+++ day1_excersices/day1_flow.py
@@ -0,0 +1,18 @@
+#here we gonna learn about loops and how they work
+#if- else
+age =20
+if age >= 18:
+    print("you are an adult")
+else:
+    print("you are a minor")
+
+#for loop
+for i in range(1,6):
+    print("square:",i**2)
+
+
+#while loop
+count=0
+while count< 3:
+    print("count:",count)
+    count+=1
