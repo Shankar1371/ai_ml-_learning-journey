f20ed076175dff05f5f814ff93492d39fc771fa2
shankar1371 <shankar.uma1371@gmail.com>
2025-09-07 20:57:36 -0500

    known how to learn a CSV data. and give quick insights of the dataset that has been used

--- Day2_excersices/day2_csv_load.py
+++ Day2_excersices/day2_csv_load.py
@@ -1,2 +1,10 @@
 import pandas as pd
-#as usual we have
+#as usual we have imported the pandas
+#and today we gonna learn about on how to load a csv file
+df =pd.read_csv("iris.csv")
+
+print("shape",df.shape)
+print("columns:",df.columns)
+print("\n first 5 rows:\n",df.head(5))
+print("\n Summary stats:\n",df.describe())
+print("\n Missing values:\n",df.isnull().sum())
