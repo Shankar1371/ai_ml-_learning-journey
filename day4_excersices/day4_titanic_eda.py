4a4850e2281f2b7104457f8d658fee3e29159761
Shankar1371 <shankar.uma1371@gmail.com>
2025-09-11 11:34:17 -0500

    learnt cleaning data

--- day4_excersices/day4_titanic_eda.py
+++ day4_excersices/day4_titanic_eda.py
@@ -15,3 +15,18 @@ print("\n columns: \n",df.columns)
 print("\n First five rows: \n",df.head(5))
 print("\n Data Types: \n",df.dtypes)
 print("\n Missing values: \n ",df.isnull().sum())
+
+
+#learning about cleaning data in the titanic
+
+#firstly  we will drop all the irrelevant columns
+df  =   df.drop(columns=["PassengerID","Name","Ticket","Cabin"])
+
+
+#now here we have to handle the missing data
+df["Age"].fillna(df["Age"].median(),inplace=True)
+#in the above line the in age indecies we are filling the null values with some of the values from the embarked and this also fills the missing values
+#.mode is used to take the most frequently used . inplace is used to modify it directly
+df["embarked"].fillna(df["Embarked"].mode()[1],inplace=True)
+
+print("\n After cleaning missing values:\n",df.isnull().sum())
